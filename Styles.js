9c74c6f4f2eeb7d97c66f071e11545d6321b5c73
OMKARHOTA <97146428+OMKARHOTA@users.noreply.github.com>
2023-01-14 15:38:11 +0530

    Update Styles.js

--- Styles.js
+++ Styles.js
@@ -1,2 +1,94 @@
+*{
+  padding:0px;
+  margin:0px;
+}
+body{
+  background:url('5651990.webp');
+
+}
+
+.navbar{
+  background-color:black;
+  border-radius: 30px;
+  
+  
+  }
+  .navbar ul{
+  overflow:auto;
+  }
+  
+  .navbar li{
+  float:left;
+  list-style: none;
+  padding:3px 3px;
+  margin:13px 20px;
+  }
+  .navbar li a{
+  padding :3px 3px;
+  text-decoration:none;
+  color:white;
+  }
+
+.break{
+  justify-content: space-between;
+}
+h1{
+  text-align: center;
+  font-family: sans-serif;
+  font-size: 50px;
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: rgb(49, 104, 104);
+}
+.cover{
+  margin: 5px;
+  width: 800px;
+  height: 400px;
+  margin-top: 50px;
+  display: flex;
+  
+  border-radius: 20%;
+  margin-left: 20px;
+}
+
+.box{
+  width: 500px;
+  height: 400px;
+  box-shadow: -1px 0 10px rgb(201 201 234);
+  padding: 50px;
+  margin-left: 20px;
+}
+img{
+  width:200px;
+  height:200px;
+  display:flex;
+}
+h2{
+  padding:2px;
+  margin:2px;
+  align-items:center;
+  font-size: larger;
+
+}
+p{
+  padding:2px;
+  font-size: medium;
+  color:white;
+  font-family :sans-serif ;
+}
+
+@media(max-width:768px){
+  .cover{
+   display: block;
+  }
+  
+
+*{
+  padding:0px;
+  margin:0px;
+}
+}
+
+
 
   
